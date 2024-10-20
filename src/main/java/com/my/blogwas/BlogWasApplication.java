bfae3770de7c34961ee6a24ae1254ed649574edd
yoonjae han <yoonjae.han@obigo.com>
2024-10-20 19:04:19 +0900

    feat: add log

--- src/main/java/com/my/blogwas/BlogWasApplication.java
+++ src/main/java/com/my/blogwas/BlogWasApplication.java
@@ -8,6 +8,7 @@ public class BlogWasApplication {
 
     public static void main(String[] args) {
         System.out.println("loood");
+
         SpringApplication.run(BlogWasApplication.class, args);
     }
 
