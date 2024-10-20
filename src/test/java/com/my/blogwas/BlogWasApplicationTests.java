61849563338dae2c7ae6188d05a826f23758d813
yoonjae han <yoonjae.han@obigo.com>
2024-10-20 18:37:50 +0900

    first commit

---
+++ src/test/java/com/my/blogwas/BlogWasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.blogwas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
